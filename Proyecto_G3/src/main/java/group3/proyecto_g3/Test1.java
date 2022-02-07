276ac68c440bb483d6bec5f03efd719a67573f96
ChristopherHG-007 <ulises7cris@hotmail.com>
2022-02-07 14:37:40 -0600

    Ejemplo de un clase y su estructura inicial

--- Proyecto_G3/src/main/java/group3/proyecto_g3/Test1.java
+++ Proyecto_G3/src/main/java/group3/proyecto_g3/Test1.java
@@ -1,12 +1,22 @@
 /*
- * Se crea una clase prueba para verificar el repositorio
+ * Test1
+ *
+ * @version 1.0
+ *
+ * Fecha 07-02-2022
+ *
+ * Copyright (c) 
  */
 package group3.proyecto_g3;
 
 /**
- *
- * @author ulise
- */
+ * Esta es la clase Test1, acá podemos encontrar la información sobre
+ * el usuario y datos de inicio de Sesión en la app.
+ *   
+ * @author      Christopher Hernandez
+ * @version     1.0     07 de Febrero 2022
+ * 
+*/
 public class Test1 {
     
 }
