0bcc40e1321aa03289d39be55cfa324cdab5b605
ChristopherHG-007 <ulises7cris@hotmail.com>
2022-02-07 15:00:19 -0600

    Ejemplo de un clase y su estructura inicial

--- Proyecto_G3/src/main/java/group3/proyecto_g3/Test1.java
+++ Proyecto_G3/src/main/java/group3/proyecto_g3/Test1.java
@@ -10,13 +10,30 @@
 package group3.proyecto_g3;
 
 /**
- * Esta es la clase Test1, acá podemos encontrar la información sobre
- * el usuario y datos de inicio de Sesión en la app.
- *   
- * @author      Christopher Hernandez
- * @version     1.0     07 de Febrero 2022
- * 
-*/
+ * Esta es la clase Test1, acá podemos encontrar la información sobre el usuario
+ * y datos de inicio de Sesión en la app.
+ *
+ * @author Christopher Hernandez
+ * @version 1.0 07 de Febrero 2022
+ *
+ */
 public class Test1 {
     
+    /**
+     * En esta clase se usarán las variables para poder identificar al cliente
+     * en el inicio de sesión. También para asegurar la dirección donde se hará
+     * el pedido, tomando en cuenta el número de celular y el nombre del
+     * cliente.
+     */
+    
+    private String test;
+    
+    /**
+     * Este sería el constructor vacío de esta clase.
+     */
+    
+    public Test1() {
+        
+    }
+
 }
